247cfd959ead7f365781f2ffa3c6de4ac3098c2d
Nurislam <nurislam.temirbekov.bj@gmail.com>
2024-03-19 15:11:25 +0600

    steps

--- practice_8/lib/page.dart
+++ practice_8/lib/page.dart
@@ -12,16 +12,118 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 10, 33, 52),
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: const Color.fromARGB(255, 85, 84, 84),
+        centerTitle: true,
+        title: const Text(
+          "Stepper",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        toolbarHeight: 60,
       ),
       body: Center(
-          child: Stepper(steps: const [
-        Step(
-          title: Text("step 1"),
-          content: Text("Inforamtion for step 1"),
-        )
-      ])),
+        child: Stepper(
+          steps: [
+            Step(
+              isActive: curentStep == 0,
+              title: const Text(
+                "step 1",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              content: const Text(
+                "information for step 1",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 226, 26, 26),
+                ),
+              ),
+            ),
+            Step(
+              isActive: curentStep == 1,
+              title: const Text("step 2",
+              style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              content: const Text(
+                "information for step 2",
+                style: TextStyle(
+                color: Color.fromARGB(255, 226, 26, 26),
+                ),
+              ),
+            ),
+            Step(
+              isActive: curentStep == 2,
+              title: const Text("step 3",
+              style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              content: const  Text(
+                "information for step 3",
+                style: TextStyle(
+                color: Color.fromARGB(255, 226, 26, 26),
+                ),
+              ),
+            ),
+            Step(
+              isActive: curentStep == 3,
+              title: const Text("step 4",
+              style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              content: const Text(
+                "information for step 5",
+                style: TextStyle(
+                color: Color.fromARGB(255, 226, 26, 26),
+                ),
+              ),
+            ),
+            Step(
+              isActive: curentStep == 4,
+              title: const Text("step 5",
+              style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              content: const Text(
+                "information for step 5",
+                style: TextStyle(
+                color: Color.fromARGB(255, 226, 26, 26),
+                ),
+              ),
+            ),
+          ],
+          onStepTapped: (int newValue) {
+            setState(() {
+              curentStep = newValue;
+            });
+          },
+        currentStep: curentStep,
+          onStepContinue: () {
+            if (curentStep != 4) {
+              setState(() {
+                curentStep += 1;
+              });
+            }
+          },
+          onStepCancel: () {
+            if (curentStep != 0) {
+              setState(() {
+                curentStep -= 1;
+              });
+            }
+          },
+          type: StepperType.vertical,
+        ),
+      ),
     );
   }
 }
--- practice_8/test/widget_test.dart
+++ practice_8/test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:practice_8/my_home_page.dart';
 
-import 'package:practice_8/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
