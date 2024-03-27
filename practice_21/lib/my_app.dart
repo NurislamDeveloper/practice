ac483759365b6345baf679fc4d3ff987d1cc3b99
Nurislam <nurislam.temirbekov.bj@gmail.com>
2024-03-27 12:34:07 +0600

    dark and light

--- practice_21/lib/my_app.dart
+++ practice_21/lib/my_app.dart
@@ -1,22 +1,44 @@
+import 'package:day_night_switcher/day_night_switcher.dart';
 import 'package:flutter/material.dart';
 
-import 'my_home_page.dart';
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkModeEnabled = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme:const  AppBarTheme(
-          backgroundColor: Colors.white,
-          centerTitle: true,
-        ),
-        scaffoldBackgroundColor: Colors.orangeAccent,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(color: Color(0xFF253341)),
+        scaffoldBackgroundColor: const Color(0xFF15202B),
       ),
-      home: const MyHomePage(),
-    );
+      themeMode: isDarkModeEnabled ? ThemeMode.dark : ThemeMode.light,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title:const  Text(
+           "Day / night swicther",
+          ),
+        ),
+        body: Center(child:DayNightSwitcher(
+        isDarkModeEnabled: isDarkModeEnabled,
+        onStateChanged: onStateChanged,
+        ),
+       ),
+      ),
+    ); 
   }
-}
+  void onStateChanged(bool isDarkModeEnabled) {
+    setState(() {
+      this.isDarkModeEnabled = isDarkModeEnabled;
+    });
+  }
+ }
